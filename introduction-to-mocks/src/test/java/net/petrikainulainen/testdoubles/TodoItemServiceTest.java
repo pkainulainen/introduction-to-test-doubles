c923f5614a6fbcdc84f497f35a285bb7be66c887
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-09-28 17:48:39 +0300

    Replace the wrong import with the correct one

--- introduction-to-mocks/src/test/java/net/petrikainulainen/testdoubles/TodoItemServiceTest.java
+++ introduction-to-mocks/src/test/java/net/petrikainulainen/testdoubles/TodoItemServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TodoItemServiceTest {
 
